a988509370fb82e47805f1a782ff395031d6dfde
kyulious <kyulikim1@gmail.com>
2021-12-26 21:09:04 +0900

    commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const submit = document.getElementById("submitbtn");
+const result1 = document.getElementById("result1");
+const input1 = document.getElementById("input");
+const section3 = document.getElementById("section3");
+
+submit.addEventListener("click", ()=>{
+    section3.style.display="inline";
+    
+})
